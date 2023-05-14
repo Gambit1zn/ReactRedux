0614457ed5a2dedb1b3584181090f2345dfc9666
Ethan Ankadu <ethan.ankadu@derivco.co.za>
2023-05-14 19:30:37 +0200

    cleaned up code by splitting slices into its own files and added basic auth state thats used in auth and header component which get conditionally rendered

--- src/store/index.js
+++ src/store/index.js
@@ -1,25 +1,7 @@
-import { createSlice, configureStore } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
+import counterReducer from './counter-slice';
+import authReducer from './auth-slice';
 
-const initialCounterState = {counter: 0, showCounter: true};
-
-const counterSlice = createSlice({
-  name: 'counter',
-  initialState: initialCounterState,
-  reducers : {
-    increment(state) {
-      state.counter++;
-    },
-    decrement(state) {
-      state.counter--
-    },
-    increase(state, action) {
-      state.counter = state.counter + action.payload;
-    },
-    toggleCounter(state) {
-      state.showCounter = !state.showCounter;
-    }
-  }
-});
 
 // const counterReducer = (state = initialState, action) => {
 //   if (action.type === 'increment') {
@@ -53,28 +35,10 @@ const counterSlice = createSlice({
 //   return state;
 // };
 
-const initialAuthState = {
-  isAuthenticated: false
-}
 
-const authSlice = createSlice({
-  name: 'authentication',
-  initialState: initialAuthState,
-  reducers: {
-    login(state) {
-      state.isAuthenticated = true;
-    },
-    logout(state) {
-      state.isAuthenticated = false;
-    }
-  }
-});
 
 const store = configureStore({
-  reducer: {counter: counterSlice.reducer, auth: authSlice.reducer}
+  reducer: {counter: counterReducer, auth: authReducer}
 });
-
-export const counterActions = counterSlice.actions;
-export const authActions = authSlice.actions;
 
 export default store;
